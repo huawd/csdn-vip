495a93c6e020ed08720a9066d4bf592b1914fa1d
huawd <1164261909@qq.com>
2020-12-29 18:00:40 +0800

    测试github

---
+++ src/test/java/com/gupaoedu/vip/pattern/singleton/test/Test1.java
@@ -0,0 +1,12 @@
+package com.gupaoedu.vip.pattern.singleton.test;
+
+/**
+ * @author Created by huawd
+ * @date 2020/12/29
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("测试github提交！");
+        
+    }
+}
