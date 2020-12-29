ed7c1a5136fd5880eef57c4b4ef3791c480d8d46
huawd <1164261909@qq.com>
2020-12-29 19:09:11 +0800

    测试github

--- src/test/java/com/gupaoedu/vip/pattern/singleton/test/Test1.java
+++ src/test/java/com/gupaoedu/vip/pattern/singleton/test/Test1.java
@@ -6,7 +6,7 @@ package com.gupaoedu.vip.pattern.singleton.test;
  */
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("测试github提交！");
+        System.out.println("测试github提交jjjjjjjjj！");
         
     }
 }
