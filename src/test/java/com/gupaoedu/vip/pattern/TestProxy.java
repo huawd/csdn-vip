5dcec672621fef4559515f14b07d8b634cb081d7
huawd <1164261909@qq.com>
2020-12-30 16:58:08 +0800

    test new branch proxy!!!!

---
+++ src/test/java/com/gupaoedu/vip/pattern/TestProxy.java
@@ -0,0 +1,11 @@
+package com.gupaoedu.vip.pattern;
+
+/**
+ * @author Created by huawd
+ * @date 2020/12/30
+ */
+public class TestProxy {
+    public static void main(String[] args) {
+        System.out.println("test proxy!!!!!!!");
+    }
+}
